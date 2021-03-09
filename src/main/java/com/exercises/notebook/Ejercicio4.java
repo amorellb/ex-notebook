298ad85c9f4e48a13de6e98bc9bbe3245bf825d5
Antonio Morell <amorellb@cifpfbmoll.eu>
2021-03-09 10:30:06 +0100

    Finished exercise 4

--- src/main/java/com/exercises/notebook/Ejercicio4.java
+++ src/main/java/com/exercises/notebook/Ejercicio4.java
@@ -1,10 +1,36 @@
 package com.exercises.notebook;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
     public static void main(String[] args) {
+        Ejercicio4 ejercicio4 = new Ejercicio4();
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write your weight (kg): ");
+        Integer weight = input.nextInt();
+        System.out.println("Write your height (cm): ");
+        Integer height = input.nextInt();
+
+        ejercicio4.calculateIMS(weight, height);
+
+        System.out.println("Your IMS is: " + ejercicio4.getIms());
+        if (ejercicio4.getIms() >= 18.0 && ejercicio4.getIms() <= 25.0) {
+            System.out.println("Your are in a good condition.");
+        }
     }
 
     private Double ims;
-    
+
+    public void calculateIMS(Integer weight, Integer height) {
+        setIms((double) weight / (((double) height / 100) * 2));
+    }
+
+    public Double getIms() {
+        return ims;
+    }
+
+    public void setIms(Double ims) {
+        this.ims = ims;
+    }
 }
