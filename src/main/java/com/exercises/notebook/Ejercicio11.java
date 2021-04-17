97fa2262cdcb80b764f32c48961f661706fee407
Antonio Morell <amorellb@cifpfbmoll.eu>
2021-04-17 20:25:57 +0200

    Finished exercise 11

--- src/main/java/com/exercises/notebook/Ejercicio11.java
+++ src/main/java/com/exercises/notebook/Ejercicio11.java
@@ -1,123 +1,46 @@
 package com.exercises.notebook;
 
-import java.util.Arrays;
-
 public class Ejercicio11 {
     public static void main(String[] args) {
-        System.out.println("No entiendo el enunciado");
 
         Ejercicio11 ejercicio11 = new Ejercicio11();
+
+        /* Inicializamos la agenda llamando al método initializeDiary y mostramos el contenido por pantalla */
+        System.out.println("Empty diary:");
+        ejercicio11.initializeDiary();
+
+        /* Añadimos algunas tareas a la agenda */
+        ejercicio11.diary[1][17] = "Comprar pan!";
+        ejercicio11.diary[3][12] = "Ir a por kikos";
+        ejercicio11.diary[0][19] = "Partidito";
+        ejercicio11.diary[6][0] = "Merendola";
+
+        /* Mostramos las tareas pendientes que ahora hay en la agenda */
+        System.out.println("\n");
+        System.out.println("Tasks to do:");
         ejercicio11.populateDiary();
-        System.out.println(Arrays.deepToString(ejercicio11.getDiary()));
+
     }
 
-    String[][] arrayBidimensional = {{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"},
-            {"01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"}};
+    /* Atributo de tipo array de string que representa la agenda */
+    private final String[][] diary = new String[7][24];
 
-    String[][] diary = new String[8][25];
-
-    public String[][] getDiary() {
-        return diary;
+    /* Método para inicializar la agenda y mostrar el contenido */
+    public void initializeDiary() {
+        for (int i = 0; i < this.diary.length; i++) {
+            for (int j = 0; j < this.diary[i].length; j++) {
+                this.diary[i][j] = "No tasks.";
+                System.out.println(diary[i][j]);
+            }
+        }
     }
 
+    /* Método para mostrar las tareas añadidas a la agenda */
     public void populateDiary() {
-        for (int i = 0; i < diary.length; i++) {
-            for (int j = 0; j < diary.length; j++) {
-                if (i == 0 && j == 0) {
-                    this.diary[i][j] = "X";
-                }
-                if (i == 0 && j == 1) {
-                    this.diary[i][j] = "00:00";
-                }
-                if (i == 0 && j == 2) {
-                    this.diary[i][j] = "01:00";
-                }
-                if (i == 0 && j == 3) {
-                    this.diary[i][j] = "02:00";
-                }
-                if (i == 0 && j == 4) {
-                    this.diary[i][j] = "03:00";
-                }
-                if (i == 0 && j == 5) {
-                    this.diary[i][j] = "04:00";
-                }
-                if (i == 0 && j == 6) {
-                    this.diary[i][j] = "05:00";
-                }
-                if (i == 0 && j == 7) {
-                    this.diary[i][j] = "06:00";
-                }
-                if (i == 0 && j == 8) {
-                    this.diary[i][j] = "07:00";
-                }
-                if (i == 0 && j == 9) {
-                    this.diary[i][j] = "08:00";
-                }
-                if (i == 0 && j == 10) {
-                    this.diary[i][j] = "09:00";
-                }
-                if (i == 0 && j == 11) {
-                    this.diary[i][j] = "10:00";
-                }
-                if (i == 0 && j == 12) {
-                    this.diary[i][j] = "11:00";
-                }
-                if (i == 0 && j == 13) {
-                    this.diary[i][j] = "12:00";
-                }
-                if (i == 0 && j == 14) {
-                    this.diary[i][j] = "13:00";
-                }
-                if (i == 0 && j == 15) {
-                    this.diary[i][j] = "14:00";
-                }
-                if (i == 0 && j == 16) {
-                    this.diary[i][j] = "15:00";
-                }
-                if (i == 0 && j == 17) {
-                    this.diary[i][j] = "16:00";
-                }
-                if (i == 0 && j == 18) {
-                    this.diary[i][j] = "17:00";
-                }
-                if (i == 0 && j == 19) {
-                    this.diary[i][j] = "18:00";
-                }
-                if (i == 0 && j == 20) {
-                    this.diary[i][j] = "19:00";
-                }
-                if (i == 0 && j == 21) {
-                    this.diary[i][j] = "20:00";
-                }
-                if (i == 0 && j == 22) {
-                    this.diary[i][j] = "21:00";
-                }
-                if (i == 0 && j == 23) {
-                    this.diary[i][j] = "22:00";
-                }
-                if (i == 0 && j == 24) {
-                    this.diary[i][j] = "23:00";
-                }
-                if (i == 1 && j == 0) {
-                    this.diary[i][j] = "Monday";
-                }
-                if (i == 2 && j == 0) {
-                    this.diary[i][j] = "Tuesday";
-                }
-                if (i == 3 && j == 0) {
-                    this.diary[i][j] = "Wednesday";
-                }
-                if (i == 4 && j == 0) {
-                    this.diary[i][j] = "Thursday";
-                }
-                if (i == 5 && j == 0) {
-                    this.diary[i][j] = "Friday";
-                }
-                if (i == 6 && j == 0) {
-                    this.diary[i][j] = "Saturday";
-                }
-                if (i == 7 && j == 0) {
-                    this.diary[i][j] = "Sunday";
+        for (int i = 0; i < this.diary.length; i++) {
+            for (int j = 0; j < this.diary[i].length; j++) {
+                if (!diary[i][j].equals("No tasks.")) {
+                    System.out.println("The day " + (i + 1) + " at " + j + ":00 you have: " + diary[i][j]);
                 }
             }
         }
